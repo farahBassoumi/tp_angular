6b1982ad58651f6ff5322b71c54f330cfaa670b2
Farah Bassoumi <bassoumiifarah@gmail.com>
2024-11-24 13:38:20 +0100

    qu

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import {
   BehaviorSubject,
   Observable,
-  concatMap,
   map,
-  takeWhile,
   scan,
+  switchMap,
+  of,
+  tap,
 } from "rxjs";
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
@@ -17,8 +18,44 @@ import { Settings } from "./dto/product-settings.dto";
   styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent {
-  /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
-  /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
-  constructor() {}
+  settings: Settings = {
+    limit: 12,
+    skip: 0,
+  };
+  productService = inject(ProductService);
+  private skip$ = new BehaviorSubject<number>(0);
+  private allProductsLoaded = false;
+  private PAGE_SIZE = 12;
+
+   // Observable for accumulating products
+   products$: Observable<Product[]> = this.skip$.pipe(
+    switchMap(skip => {
+      if (this.allProductsLoaded) return of([]); // Prevent further API calls
+      return this.productService.getProducts({ limit: this.PAGE_SIZE, skip }).pipe(
+        //pipe listens for changes and fetch the corresponding products
+        tap(products => {
+          if (products.total < this.PAGE_SIZE) {
+            this.allProductsLoaded = true; // Mark as loaded when fewer products are returned
+          }
+        
+        }),
+        map(response => response.products) // Extract the array of products
+
+      );
+    }),
+    scan<Product[], Product[]>(
+      (allProducts, newProducts) => [...allProducts, ...newProducts], []) // Accumulate products
+  );
+
+constructor() { }
+
+
+loadMore(){;
+  if(!this.allProductsLoaded){
+    this.skip$.next(this.skip$.value + this.PAGE_SIZE);
+  }
 }
+
+
+}
+
